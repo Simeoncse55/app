c1df8f0878bc73d252e653ec1f40771bdfe177b7
VEKGADESH <VEKGADESH@VENGADESH>
2022-08-28 16:35:46 +0530

    just practise for commit in github

---
+++ vengadesh/src/vengadesh/demo.java
@@ -0,0 +1,11 @@
+package vengadesh;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello !  this is a demo message !! ");
+
+	}
+
+}
